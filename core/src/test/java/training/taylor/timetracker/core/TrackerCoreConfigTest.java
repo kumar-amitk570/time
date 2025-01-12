d0412dcfac6891913fbb508bebccd16acb5d3f57
Amit <kumar-amitk@outlook.com>
2025-01-12 11:14:49 +0530

    Update TrackerCoreConfigTest.java

--- core/src/test/java/training/taylor/timetracker/core/TrackerCoreConfigTest.java
+++ core/src/test/java/training/taylor/timetracker/core/TrackerCoreConfigTest.java
@@ -11,19 +11,35 @@ import training.taylor.timetracker.core.dao.TimeEntry;
 
 import java.util.List;
 
-/**
- * Created by Jason on 6/20/2015.
- */
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = TrackerCoreConfig.class)
 public class TrackerCoreConfigTest {
 
     @Autowired
-    List<TimeEntry> entries;
+    private List<TimeEntry> entries;
 
     @Test
-    public void testMe() {
-        assertNotNull(entries);
+    public void testEntriesNotNull() {
+        // Verify that the entries list is successfully injected and not null
+        assertNotNull("Entries list should not be null", entries);
     }
 
+    @Test
+    public void testAddTimeEntry() {
+        // Add a TimeEntry and verify the size of the list
+        TimeEntry entry = new TimeEntry();
+        entries.add(entry);
+        assertEquals("Entries list should contain 1 element", 1, entries.size());
+        assertSame("The added entry should be in the list", entry, entries.get(0));
+    }
+
+    @Test
+    public void testRemoveTimeEntry() {
+        // Add and remove a TimeEntry, and verify the list is empty
+        TimeEntry entry = new TimeEntry();
+        entries.add(entry);
+        entries.remove(entry);
+        assertTrue("Entries list should be empty after removing the entry", entries.isEmpty());
+    }
 }
+
